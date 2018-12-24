1bca8643fa32537e21abc3525d8dae024c0e6f9b
H122984 <rahul.deep@honeywell.com>
2018-12-24 16:53:37 +0530

    First Commit - 24th Dec 4:54PM

---
+++ CvMaker/src/main/java/com/rhuldip/CvMakerApplication.java
@@ -0,0 +1,14 @@
+package com.rhuldip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvMakerApplication.class, args);
+	}
+
+}
+
---
+++ CvMaker/src/test/java/com/rhuldip/CvMakerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rhuldip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CvMakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
